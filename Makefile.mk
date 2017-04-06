f0dee6d55c08a9d9c8884a9f1189f532c7b73ffb
Stephen Heumann <stephenheumann@gmail.com>
2017-04-05 21:22:18 -0500

    Various makefile updates.
    It is now set up to use the stock ORCA libraries and to enable most optimizations.

--- Makefile.mk
+++ Makefile.mk
@@ -1,4 +1,8 @@
-CFLAGS = -i -w
+# Use stock ORCA libraries & headers, not GNO ones
+USEORCALIBS = prefix 13 /lang/orca/Libraries
+COMMAND = $(!eq,$(CMNDNAME),$(CC) $(CMNDNAME) $(USEORCALIBS)&&$(CC)) $(CMNDARGS)
+
+CFLAGS = -i -w -O95
 
 DSITEST_OBJS = dsitest.o aspinterface.o dsi.o readtcp.o endian.o tcpconnection.o atipmapping.o asmglue.o cmdproc.o
 DSITEST_PROG = dsitest
@@ -10,7 +14,7 @@ DUMPCMDTBL_OBJS = dumpcmdtbl.o asmglue.o
 DUMPCMDTBL_PROG = dumpcmdtbl
 
 AFPBRIDGE_OBJS = afpinit.o afpbridge.o aspinterface.o dsi.o readtcp.o endian.o tcpconnection.o atipmapping.o asmglue.o installcmds.o cmdproc.o callat.o
-AFPBRIDGE_PROG = afpbridge
+AFPBRIDGE_PROG = AFPBridge
 
 PROGS = $(DSITEST_PROG) $(AFPMOUNTER_PROG) $(DUMPCMDTBL_PROG) $(AFPBRIDGE_PROG)
 
@@ -18,16 +22,16 @@ PROGS = $(DSITEST_PROG) $(AFPMOUNTER_PROG) $(DUMPCMDTBL_PROG) $(AFPBRIDGE_PROG)
 default: $(PROGS)
 
 $(DSITEST_PROG): $(DSITEST_OBJS)
-	occ $(CFLAGS) -o $@ $(DSITEST_OBJS)
+	$(CC) $(CFLAGS) -o $@ $(DSITEST_OBJS)
 
 $(AFPMOUNTER_PROG): $(AFPMOUNTER_OBJS)
-	occ $(CFLAGS) -o $@ $(AFPMOUNTER_OBJS)
+	$(CC) $(CFLAGS) -o $@ $(AFPMOUNTER_OBJS)
 
 $(DUMPCMDTBL_PROG): $(DUMPCMDTBL_OBJS)
-	occ $(CFLAGS) -o $@ $(DUMPCMDTBL_OBJS)
+	$(CC) $(CFLAGS) -o $@ $(DUMPCMDTBL_OBJS)
 
 $(AFPBRIDGE_PROG): $(AFPBRIDGE_OBJS)
-	occ $(CFLAGS) -o $@ $(AFPBRIDGE_OBJS)
+	$(CC) $(CFLAGS) -M -o $@ $(AFPBRIDGE_OBJS) > $@.map
 	chtyp -tpif $@
 
 %.macros: %.asm
@@ -37,6 +41,13 @@ $(AFPBRIDGE_PROG): $(AFPBRIDGE_OBJS)
 install: $(AFPBRIDGE_PROG)
 	cp $(AFPBRIDGE_PROG) "*/System/System.Setup"
 
+.PHONY: import
+import:
+	chtyp -ttxt *.mk
+	chtyp -lcc *.c *.h
+	chtyp -lasm *.asm *.macros
+	udl -g *
+
 .PHONY: clean
 clean:
-	$(RM) $(PROGS) *.o *.root > .null
+	$(RM) $(PROGS) *.o *.root *.map > .null
